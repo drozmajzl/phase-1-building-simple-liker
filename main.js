97fd1b687a920b5e8c38e5f66c5679033169141d
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-01-23 21:15:47 -0500

    commit tree d5aa56a6c4c2668599695510d915352e7f269fe5

--- main.js
+++ main.js
@@ -4,8 +4,51 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//DECLARE GLOBAL CONSTANTS
+const errorModal = document.querySelector('#modal')
+const h2 = document.querySelector('#modal').children[0]
+const heartClass = document.getElementsByClassName('like')
+Array.from(heartClass).forEach(enableLike)
 
 
+//EVENT LISTENER FUNCTION
+function enableLike (li) {
+li.addEventListener('click', () => likeButton(li))
+}
+
+//LIKE BUTTON FUNCTIONALITY
+function likeButton(li){
+  mimicServerCall("http://mimicServer.example.com", configuration)
+  .then(res => liker(li, res))
+  .catch(function (error) {
+    errorModal.className = 'visible'
+    h2.textContent = `${error}`
+    console.log(error);
+    setTimeout((() => errorModal.className ='hidden'),3000);
+  });
+}
+
+function liker (li, res) {
+  console.log(res)
+  if(li.textContent === 'Like! ♡'){
+    li.children[0].textContent = FULL_HEART
+    li.children[0].className = 'activated-heart'
+  }
+  else{
+    li.children[0].textContent = EMPTY_HEART
+    li.children[0].className = 'like'
+  }
+}
+
+const configuration = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+  }),
+};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +66,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
